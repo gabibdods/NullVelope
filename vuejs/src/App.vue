7f67747f49cb26686b44541326fa004a7305dae8
gabri <gabri@NullVelope.home>
2025-08-03 00:27:08 +0000

    SMTP Server configured; FastAPI Server configured; Vue.js App configured

---
+++ vuejs/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1 class="text-xl font-bold text-center mb-8">Inbox</h1>
+        <ul>
+            <li v-for="email in emails" :key="email._id" class="mt-2 border p-2">
+                <p><strong>From:</strong> {{ email.from }}</p>
+                <p><strong>Subject:</strong> {{ email.subject }}</p>
+                <p><strong>Body:</strong> {{ email.txt }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref, onMounted } from 'vue'
+    import axios from 'axios'
+
+    const emails = ref([])
+    onMounted(async () => {
+        const res = await axios.get('http://localhost:8000/inbox/{inboxId}')
+        emails.value = res.data
+    })
+</script>
+
+<style scoped>
+
+</style>
